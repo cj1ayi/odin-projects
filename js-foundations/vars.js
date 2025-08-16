7d943b030ecb3688d6173071e578b7ef53d37b2f
Nate Tan <69299982+cj1ayi@users.noreply.github.com>
2025-08-17 00:34:03 +0800

    Complete rock paper scissors

--- js-foundations/vars.js
+++ js-foundations/vars.js
@@ -1,6 +1,29 @@
-let name = "John";
-let admin;
+let num = 5
+let a = 2 
+let b = 8
+let str = "bANANA"
 
-admin = name;
+function add7(num) {return num + 7;}
 
-alert(admin)
+
+function multiply(a, b) {return a * b;}
+
+function capitalize(str)
+{
+    if (str.length === 0)
+        return "String is empty"
+    return str.charAt(0).toUpperCase()
+}
+
+function lastLetter(str)
+{
+    if (str.length === 0)
+    return "String is empty"
+    return str.charAt(str.length - 1)
+}
+
+
+console.log(add7(5));           // 12
+console.log(multiply(2, 8));    // 16
+console.log(capitalize("bANANA")); // "B"
+console.log(lastLetter("abcd"));   // "d"
